1b9f85af190930f88d1eba61a29693e4fdcf236a
Adam J. Jackson <a.j.jackson@physics.org>
2019-05-30 10:09:42 +0100

    Refactor all non-numrow symbols to RH and LOWER

--- keyboards/keebio/nyquist/keymaps/ajjackson/keymap.c
+++ keyboards/keebio/nyquist/keymaps/ajjackson/keymap.c
@@ -89,22 +89,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  |   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  \   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |      |      |      |      |      |      |   _  |   +  |   {  |   }  |   |  |
+ * | Del  |      |      |      |      |      |   -  |   _  |   +  |   {  |   }  |ISO # |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      | C-z  | C-x  | C-c  | C-v  |      |      |ISO ~ |ISO | |      |      |      |
+ * |      | C-z  | C-x  | C-c  | C-v  |      |   =  |ISO ~ |ISO | |   [  |   ]  |ISO \ |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_TILD, KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_TILD, KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
-  KC_DEL,  _______,    _______,    _______,    _______,    _______, _______, KC_UNDS,    KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, LCTL(KC_C), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______, _______, S(KC_NUHS), S(KC_NUBS),_______, _______, _______, \
+  KC_TILD, KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
+  KC_TILD, KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS, \
+  KC_DEL,  _______,    _______,    _______,    _______,    _______, KC_MINS, KC_UNDS,    KC_PLUS, KC_LCBR, KC_RCBR, KC_NUHS, \
+  _______, LCTL(KC_C), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______, KC_EQL,  S(KC_NUHS), S(KC_NUBS), KC_LBRC, KC_RBRC, KC_NUBS, \
   _______, _______,    _______,    _______,    _______,    _______, _______, _______,    KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO \ |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -124,8 +124,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______, \
+  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, _______, _______, _______, \
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
